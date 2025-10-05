00582a153912a5305f19a43362ce3afedbb57378
Aditya kumar Guota <deadpoolxadi@gmail.com>
2025-10-04 22:53:13 -0500

    Add files via upload

---
+++ Ass02ps3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int hours, minutes, totalMinutes;
+
+    printf("Enter hours: ");
+    scanf("%d", &hours);
+
+    printf("Enter minutes: ");
+    scanf("%d", &minutes);
+
+    totalMinutes = (hours * 60) + minutes;
+
+    printf("Total: %d minutes\n", totalMinutes);
+
+    return 0;
+}
